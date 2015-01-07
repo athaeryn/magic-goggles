fb1754b1643d24b266dd75d68f4bb7152a9c7740
Mike Anderson <hello@mrmikea.com>
2015-01-07 03:01:22 -0600

    Print all the fuzzywuzzy guesses to stderr

--- title_guesser.py
+++ title_guesser.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import sys
+
 from fuzzywuzzy import process
 
 from card_names import load_card_names
@@ -8,4 +11,6 @@ class TitleGuesser:
         self._card_names = load_card_names(db_path)
 
     def guess(self, crap_title):
-        return process.extractOne(crap_title, self._card_names)[0]
+        titles = process.extract(crap_title, self._card_names)
+        print(titles, file=sys.stderr)
+        return titles[0][0]
